c84d13a5c4983541f89b6ab31dd9cdaad258ea1a
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-08-21 09:44:19 +0200

    .

--- Generate ReleaseNotes.bat
+++ Generate ReleaseNotes.bat
@@ -2,4 +2,4 @@ rem https://github.com/StefH/GitHubReleaseNotes
 
 SET version=11.0.0
 
-GitHubReleaseNotes --output ReleaseNotes.md --skip-empty-releases --exclude-labels question invalid documentation --version %version%
+GitHubReleaseNotes --output ReleaseNotes.md --exclude-labels question invalid documentation --version %version%
